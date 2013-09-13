ea4fc404d4d4e7eb85eb4d5fc88bdf894a4ecfa7
tylerd <tylergd@outlook.com>
2013-09-13 00:29:36 +0000

    Adding Feed Loading Code

--- Source/CloudFeed/Controllers/HomeController.cs
+++ Source/CloudFeed/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CloudFeed.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +12,8 @@ namespace CloudFeed.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Feeds = Feeds.GetFeeds(Server.MapPath("~/Feeds.txt"));
+
             return View();
         }
 
---
+++ Source/CloudFeed/Models/Feeds.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using System.Xml;
+
+namespace CloudFeed.Models
+{
+    public class Feeds
+    {
+        public static IEnumerable<Uri> GetFeeds(string path)
+        {
+            string line;
+            var feeds = new List<Uri>();
+
+            StreamReader file = new StreamReader(path);
+
+            while ((line = file.ReadLine()) != null)
+            {
+                feeds.Add(new Uri(line));
+            }
+
+            file.Close();
+
+            return feeds;
+        }
+
+        public static IEnumerable<SyndicationItem> GetFeedItems(Uri feed)
+        {
+            var r = XmlReader.Create(feed.ToString());
+            var feedData = SyndicationFeed.Load(r);
+            r.Close();
+            return feedData.Items;
+        }
+    }
+}
